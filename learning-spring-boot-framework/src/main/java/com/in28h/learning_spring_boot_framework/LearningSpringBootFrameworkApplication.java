c7de81702fda5d24bbde361f0044303accc90a39
mortada1199 <murtada199815@gmail.com>
2025-10-28 18:47:36 +0200

    first commit

---
+++ learning-spring-boot-framework/src/main/java/com/in28h/learning_spring_boot_framework/LearningSpringBootFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.in28h.learning_spring_boot_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootFrameworkApplication.class, args);
+	}
+
+}
---
+++ learning-spring-boot-framework/src/test/java/com/in28h/learning_spring_boot_framework/LearningSpringBootFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28h.learning_spring_boot_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
